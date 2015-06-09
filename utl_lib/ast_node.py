d2d8153cb3364d4a87229effb80b3a4c2b86f05b
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-06-09 16:34:25 -0400

    Add attributes, __str__()

--- utl_lib/ast_node.py
+++ utl_lib/ast_node.py
@@ -8,9 +8,25 @@ class ASTNode(object):
         self.children = []
         self.symbol = symbol_name
         self.terminal = True
+        self.attributes = {}
 
     def add_child(self, a_child):
         self.children.append(a_child)
 
     def add_children(self, iterator):
         self.children.extend(iterator)
+
+    def __str__(self):
+        result = 'Node({})'.format(self.symbol)
+        if self.attributes:
+            attrs = ''
+            for key in self.attributes:
+                if attrs:
+                    attrs += ', {}: "{}"'.format(key, self.attributes[key])
+                else:
+                    attrs = '{}: "{}"'.format(key, self.attributes[key])
+            result += " [{}]".format(attrs)
+
+        for child in self.children:
+            result += ' -> {}'.format(child)
+        return result
