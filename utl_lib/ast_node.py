93b03d9156909f4628d6f0748e7f6ebc26acb9cf
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-06-10 18:20:03 -0400

    Added ASTNodeFormatter, ASTNode.__repr__, params to __init__

--- utl_lib/ast_node.py
+++ utl_lib/ast_node.py
@@ -2,19 +2,38 @@
 
 
 class ASTNode(object):
-    """A node in the AST tree. """
+    """A node in the AST tree.
 
-    def __init__(self, symbol_name):
-        self.children = []
+    :param str symbol_name: A name for the node, usually related to the rule that produced it.
+
+    :param Boolean is_term: :py:attr:`True` if this is a terminal node (never has children).
+
+    :param dict attrs: key-value pairs attaching arbitrary attributes to this node. For
+        attributes that are not common to all nodes.
+
+    :param list children: iterator of nodes, which will be attached as children.
+
+    """
+
+    def __init__(self, symbol_name, is_term, attrs=None, children=None):
+        self.children = [] if children is None else children
+        self.parent = None  # set by parent in add_child
         self.symbol = symbol_name
-        self.terminal = True
-        self.attributes = {}
+        self.terminal = is_term
+        self.attributes = {} if attrs is None else attrs
 
-    def add_child(self, a_child):
-        self.children.append(a_child)
+    def add_child(self, child):
+        # we might have passed a generator to __init__(), or even a set
+        self.children = list(self.children)
+        child.parent = self
+        self.children.append(child)
 
     def add_children(self, iterator):
-        self.children.extend(iterator)
+        # we might have passed a generator to __init__(), or even a set
+        self.children = list(self.children)
+        for child in iterator:
+            if child:
+                self.add_child(child)
 
     def __str__(self):
         result = 'Node({})'.format(self.symbol)
@@ -22,11 +41,33 @@ class ASTNode(object):
             attrs = ''
             for key in self.attributes:
                 if attrs:
-                    attrs += ', {}: "{}"'.format(key, self.attributes[key])
+                    attrs += ', {}: "{}"'.format(key, repr(self.attributes[key]))
                 else:
-                    attrs = '{}: "{}"'.format(key, self.attributes[key])
-            result += " [{}]".format(attrs)
+                    attrs = '{}: "{}"'.format(key, repr(self.attributes[key]))
+            result += " {%s}" % attrs
+        return result
 
+    def __repr__(self):
+        child_list = ""
         for child in self.children:
-            result += ' -> {}'.format(child)
+            if child_list:
+                child_list += ", {}".format(child.symbol)
+            else:
+                child_list = child.symbol
+        return'ASTNode("{}", {}, ..., [{}])'.format(self.symbol,
+                                                    "True" if self.terminal else "False",
+                                                    child_list)
+
+
+class ASTNodeFormatter(object):
+
+    def __init__(self, root_node):
+        self.root = root_node
+
+    def format(self):
+        result = str(self.root)
+        for child in self.root.children:
+            lines = ASTNodeFormatter(child).format().split('\n')
+            for line in lines:
+                result += '\n    ' + line
         return result
