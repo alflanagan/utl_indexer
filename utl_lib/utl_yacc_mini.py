ee0997d293de839079f08696bd8d39c8611b25a9
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-06-11 17:04:29 -0400

    Multiple fixes to resolve rule problems Generate AST Nodes for most rules done

--- utl_lib/utl_yacc_mini.py
+++ utl_lib/utl_yacc_mini.py
@@ -41,10 +41,10 @@ class UTLParser(object):  # pylint: disable=too-many-public-methods
                   | document_or_code'''
         self.parsed = True
         if len(p) == 2:
-            # p[1] is 'statement_list'
-            p[0] =  ASTNode('root', False, {}, [p[1]])
+            assert p[1].symbol == 'statement_list'
+            p[0] = ASTNode('root', False, {}, [p[1]])
         else:
-            # p[1] is 'root'
+            assert p[1].symbol == 'root'
             p[1].add_children(p[2:])
             p[0] = p[1]
 
@@ -56,66 +56,122 @@ class UTLParser(object):  # pylint: disable=too-many-public-methods
         if len(p) == 2:
             newnode = ASTNode('document', True, {'text': p[1]})
         else:
+            assert p[2].symbol == 'statement_list'
             newnode = p[2]
         p[0] = newnode
 
     def p_statement_list(self, p):
-        '''statement_list : statement_list statement
+        '''statement_list : statement statement_list
                           | statement'''
-        p[0] = ASTNode('statement_list', False, {}, p[1:])
+        assert p[1].symbol == 'statement'
+        if len(p) == 2:
+            p[0] = ASTNode('statement_list', False, {}, [p[1]])
+        else:
+            assert p[2].symbol == 'statement_list'
+            p[2].add_child(p[1])
+            p[0] = p[2]
 
     def p_statement(self, p):
         '''statement : expr SEMI
                      | assignment SEMI'''
+        assert p[1].symbol in ['expr', 'assignment']
         p[0] = ASTNode('statement', False, {}, [p[1]])
 
     def p_expr(self, p):
         '''expr : expr PLUS term
                 | expr MINUS term
                 | term
-                | FILTER method_call
+                | expr FILTER method_call
+                | expr FILTER ID
 
                 '''
-        p[0] = ASTNode('expr', False)
+        if len(p) == 4:
+            if isinstance(p[3], str):
+                assert p[1] and p[2]
+                new_id = ASTNode('id', True, {'name': p[3],}, [])
+                p[0] = ASTNode('expr', False, {'operator': p[2],}, [p[1], new_id])
+            else:
+                assert p[1] and p[2] and p[3]
+                p[0] = ASTNode('expr', False, {"operator": p[2]}, [p[1], p[3]])
+        else:
+            p[0] = ASTNode('expr', False, {}, p[1:])
 
     # convenience rule to explicitly state production to nothing
     def p_empty(self, p):
         '''empty :'''
-        pass
+        # you'd think we could just use None, but not good idea
+        p[0] = ASTNode('empty', True)
 
     def p_param_list(self, p):
         '''param_list : param_decl
                       | param_list COMMA param_decl
                       | empty'''
-        pass
+        if p[1].symbol == 'empty':
+            p[0] = p[1]  # why create another?
+        elif p[1].symbol == 'param_decl':
+            p[0] = ASTNode('param_list', False, {}, [p[1]])
+        else:
+            assert p[1].symbol == 'param_list'
+            assert p[3].symbol == 'param_decl'
+            p[1].add_child(p[3])
+            p[0] = p[1]
 
     def p_param_decl(self, p):
         '''param_decl : ID
                       | assignment'''
-        pass
+        if isinstance(p[1], str):
+            p[0] = ASTNode('param_decl', True, {'name': p[1],})
+        else:
+            assert p[1].symbol == 'assignment'
+            p[0] = ASTNode('param_decl', False, {'name': p[1].attributes['target'],},
+                           p[1].children)
 
     def p_assignment(self, p):
         '''assignment : ID ASSIGN expr'''
-        p[0] = ASTNode('assignment', False)
+        p[0] = ASTNode('assignment', False, {'target': p[1],}, [p[3]])
 
     def p_method_call(self, p):
         '''method_call : ID LPAREN param_list RPAREN'''
-        pass
+        p[0] = ASTNode('call', False, {'name': p[1],}, p[3])
 
     def p_term(self, p):
         '''term : term TIMES factor
                 | term DIV factor
                 | term MODULUS factor
                 | factor'''
-        pass
+        if len(p) == 2:
+            p[0] = ASTNode('term', False, {}, [p[1]])
+        else:
+            p[0] = ASTNode('term', False, {'operator': p[2],}, [p[1], p[3]])
 
     def p_factor(self, p):
-        '''factor : NUMBER
-                  | ID LBRACKET expr RBRACKET
-                  | ID
+        '''factor : literal
+                  | id_ref
                   | LPAREN expr RPAREN
                   | method_call'''
-        pass
+        # odd to have string here, but "5" can auto-convert to 5.0, so it's legal
+        if len(p) == 2:
+            assert p[1]
+            p[0] = ASTNode('factor', False, {}, [p[1]])
+        else:
+            assert p[1] == '(' and p[3] == ')'
+            assert p[2]
+            p[0] = ASTNode('paren_group', False, {}, [p[2]])
+
+    def p_literal(self, p):
+        '''literal : NUMBER
+                   | STRING'''
+        p[0] = ASTNode('literal', True, {'value': p[1],}, [])
+
+    #exists because it's easier than trying to identify ID in p_factor()
+    def p_id_ref(self, p):
+        '''id_ref : ID LBRACKET expr RBRACKET
+                  | ID'''
+        if len(p) == 2:
+            p[0] = ASTNode('identifier', True, {'name': p[1],})
+        else:
+            assert p[2] == "[" and p[4] == "]"
+            p[0] = ASTNode('array-ref', False, {'name': p[1],}, [p[3]])
 
     # Error rule for syntax errors
     def p_error(self, p):  # pylint: disable=missing-docstring
