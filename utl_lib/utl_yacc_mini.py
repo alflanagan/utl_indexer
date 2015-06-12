ee0c37663bc2012ba601c4650ce3fd079c90c97c
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-06-12 17:03:30 -0400

    added p_arg_list(), p_arg(), fixed p_method_call() removed p_empty() renamed 'root' node to 'utldoc'

--- utl_lib/utl_yacc_mini.py
+++ utl_lib/utl_yacc_mini.py
@@ -42,9 +42,9 @@ class UTLParser(object):  # pylint: disable=too-many-public-methods
         self.parsed = True
         if len(p) == 2:
             assert p[1].symbol in ('statement_list', 'document')
-            p[0] = ASTNode('root', False, {}, [p[1]])
+            p[0] = ASTNode('utldoc', False, {}, [p[1]])
         else:
-            assert p[1].symbol == 'root'
+            assert p[1].symbol == 'utldoc'
             p[1].add_children(p[2:])
             p[0] = p[1]
 
@@ -54,28 +54,25 @@ class UTLParser(object):  # pylint: disable=too-many-public-methods
         # attributes of p:
         # error, lexer, lexpos, lexspan, lineno, linespan, parser, set_lineno, slice, stack
         if len(p) == 2:
-            newnode = ASTNode('document', True, {'text': p[1]})
+            p[0] = ASTNode('document', True, {'text': p[1]})
         else:
             assert p[2].symbol == 'statement_list'
-            newnode = p[2]
-        p[0] = newnode
+            p[0] = p[2]
 
     def p_statement_list(self, p):
-        '''statement_list : statement statement_list
+        '''statement_list : statement_list statement
                           | statement'''
-        assert p[1].symbol == 'statement'
         if len(p) == 2:
             p[0] = ASTNode('statement_list', False, {}, [p[1]])
         else:
-            assert p[2].symbol == 'statement_list'
-            p[2].add_child(p[1])
-            p[0] = p[2]
+            assert p[1].symbol == 'statement_list'
+            p[1].add_child(p[2])
+            p[0] = p[1]
 
     def p_statement(self, p):
         '''statement : expr SEMI
                      | assignment SEMI
                      | simple_if_stmt SEMI'''
-        assert p[1].symbol in ['expr', 'assignment']
         p[0] = ASTNode('statement', False, {}, [p[1]])
 
     def p_expr(self, p):
@@ -98,19 +95,13 @@ class UTLParser(object):  # pylint: disable=too-many-public-methods
         else:
             p[0] = ASTNode('expr', False, {}, p[1:])
 
-    # convenience rule to explicitly state production to nothing
-    def p_empty(self, p):
-        '''empty :'''
-        # you'd think we could just use None, but not good idea
-        p[0] = ASTNode('empty', True)
-
     def p_param_list(self, p):
-        '''param_list : param_decl
+        '''param_list :
                       | param_list COMMA param_decl
-                      | empty'''
-        if p[1].symbol == 'empty':
-            p[0] = p[1]  # why create another?
-        elif p[1].symbol == 'param_decl':
+                      | param_decl '''
+        if len(p) == 1:
+            p[0] = ASTNode('param_list', True)
+        elif len(p) == 2:
             p[0] = ASTNode('param_list', False, {}, [p[1]])
         else:
             assert p[1].symbol == 'param_list'
@@ -128,13 +119,35 @@ class UTLParser(object):  # pylint: disable=too-many-public-methods
             p[0] = ASTNode('param_decl', False, {'name': p[1].attributes['target']},
                            p[1].children)
 
+    def p_arg_list(self, p):
+        '''arg_list :
+                    | arg
+                    | arg_list COMMA arg'''
+        if len(p) == 1:
+            p[0] = ASTNode('arg_list', True, {}, [])
+        elif len(p) == 2:
+            p[0] = ASTNode('arg_list', False, {}, [p[1]])
+        else:
+            assert p[1].symbol == "arg_list"
+            p[1].add_child(p[3])
+            p[0] = p[1]
+
+    def p_arg(self, p):
+        '''arg : expr
+               | STRING COLON expr'''
+        if len(p) == 4:
+            assert p[2] == ':'
+            p[0] = ASTNode('arg', False, {'keyword': p[1]}, [p[3]])
+        else:
+            p[0] = ASTNode('arg', False, {}, [p[1]])
+
     def p_assignment(self, p):
         '''assignment : ID ASSIGN expr'''
         p[0] = ASTNode('assignment', False, {'target': p[1]}, [p[3]])
 
     def p_method_call(self, p):
-        '''method_call : ID LPAREN param_list RPAREN'''
-        p[0] = ASTNode('call', False, {'name': p[1]}, p[3])
+        '''method_call : ID LPAREN arg_list RPAREN'''
+        p[0] = ASTNode('method_call', False, {'name': p[1]}, [p[3]])
 
     def p_term(self, p):
         '''term : term TIMES factor
