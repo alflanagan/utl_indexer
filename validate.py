fdf8201aad3930f6916fc6901f5ef3284f69a972
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-06-29 16:48:41 -0400

    Added success message, improved error message

--- validate.py
+++ validate.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Script to run parser over a UTL file, solely to check syntax."""
-
+import sys
 import argparse
 
 from utl_lib.utl_yacc import UTLParser
 # parent class does nothing, which is exactly what we want
-from utl_lib.utl_parse_handler import UTLParseHandler
+from utl_lib.utl_parse_handler import UTLParseHandler, UTLParseError
 
 
 def get_args():
@@ -21,8 +21,13 @@ def get_args():
 def main(args):
     """Main function. Opens file, parses it."""
     myparser = UTLParser([UTLParseHandler()])
-    myparser.parse(args.utl_file.read(), debug=args.debug)
-
+    try:
+        myparser.parse(args.utl_file.read(), debug=args.debug)
+        print("{} appears valid.".format(args.utl_file.name))
+    except UTLParseError as upe:
+        sys.stderr.write("File {} failed validation!\n".format(args.utl_file.name))
+        sys.stderr.write("{}\n".format(upe))
+        sys.exit(1)
 
 if __name__ == '__main__':
     main(get_args())
