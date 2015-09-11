a77d2a026eca73d9f533d827479c7f67b244d400
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-09-11 16:42:20 -0400

    Use utl_yacc.py for comparison, not temp copy.

--- match_handler_yacc.sh
+++ match_handler_yacc.sh
@@ -10,7 +10,7 @@ TMPFIL2=$(mktemp)
 TMPFIL3=$(mktemp)
 trap "{ rm -f ${TMPFIL} ${TMPFIL2} ${TMPFIL3}; }" EXIT INT KILL
 
-grep -v '^#' utl_lib/utl_yacc2.py | grep -oh -e 'def p_[^(]*' | sed -e 's/def p_//' | sort > ${TMPFIL}
+grep -v '^#' utl_lib/utl_yacc.py | grep -oh -e 'def p_[^(]*' | sed -e 's/def p_//' | sort > ${TMPFIL}
 grep -v '^#' $1 | grep -oh -e 'def [^_][^(]*' | sed -e 's/def //' | sort > ${TMPFIL2}
 
 diff ${TMPFIL} ${TMPFIL2} > ${TMPFIL3}
