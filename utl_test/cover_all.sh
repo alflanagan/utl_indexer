e99c1037458364bd511208883003ff679566cca1
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-05-24 18:34:32 -0400

    update comment

--- utl_test/cover_all.sh
+++ utl_test/cover_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env dash
 
 # modules that don't have test suites (yet):
-# handler_parse_tree handler_print_productions handler_null
+# handler_parse_tree handler_print_productions handler_null tn_site.py utl_lex_comments.py
 
 for MODULE in utl_parse_test ast_node utl_lex utl_yacc handler_ast macro_xref tn_package
 do
