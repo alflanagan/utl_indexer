c53616957822fac612e4f4a6b398838be1aa2ca2
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-05-25 15:25:11 -0400

    Added init params

--- utl_test/test_tn_package.py
+++ utl_test/test_tn_package.py
@@ -21,11 +21,15 @@ class TNPackageTestCase(unittest_plus.TestCasePlus):
         """Unit test for :py:meth:`tn_package.TNPackage`. Verify public properties are set."""
         item1 = TNPackage({"app": "fred", "version": "1.2.3", "name": "some_package",},
                           True,
-                          {"some_other_package": "3.4.5",})
+                          {"some_other_package": "3.4.5",},
+                          "downloaded/zippped/some_package_1.2.3.zip",
+                          "http://package.example.com")
         self.assertEqual(item1.name, "some_package")
         self.assertEqual(item1.app, "fred")
         self.assertEqual(item1.version, "1.2.3")
         self.assertEqual(item1.is_certified, True)
+        self.assertEqual(item1.site, "http://package.example.com")
+        self.assertEqual(item1.zipfile, "downloaded/zippped/some_package_1.2.3.zip")
         self.assertDictEqual(item1.deps, {"some_other_package": "3.4.5",})
         self.assertDictEqual(item1.properties, {"app": "fred", "version": "1.2.3",
                                                 "name": "some_package",})
