48a364c8a1f90d4ece3cc09a8e02a7eb7e52c0fd
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-07-21 17:43:28 -0400

    Add failing test cases for extra commas

--- utl_test/cover_all.sh
+++ utl_test/cover_all.sh
@@ -3,7 +3,7 @@
 # modules that don't have test suites (yet):
 # handler_parse_tree handler_print_productions handler_null tn_site.py utl_lex_comments.py
 
-for MODULE in utl_parse_test ast_node utl_lex utl_yacc handler_ast macro_xref tn_package
+for MODULE in ast_node handler_ast macro_xref tn_package utl_lex utl_parse_test utl_yacc
 do
   echo "************************************** ${MODULE} **************************************"
   ./cover_test.sh ${MODULE}
